5591332d45e029371c10799f481dca54b3d4364f
Dan Steren <dan@dansteren.com>
2024-01-12 12:35:31 -0700

    Improve empty state of ReviewList

--- apps/reviews-client/src/app/components/reviews-list/reviews-list.tsx
+++ apps/reviews-client/src/app/components/reviews-list/reviews-list.tsx
@@ -1,6 +1,6 @@
 import Review from '../review/review';
 import useFetch from '../../hooks/useFetch';
-import { LinearProgress } from '@mui/material';
+import { LinearProgress, Rating } from '@mui/material';
 
 /* eslint-disable-next-line */
 export interface ReviewsListProps {}
@@ -10,6 +10,17 @@ export function ReviewsList(props: ReviewsListProps) {
 
 	if (loading) return <LinearProgress />;
 	if (error) return <p>error: {error}</p>;
+
+	if (reviews.length === 0) {
+		return (
+			<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+				<Rating name="no-value" value={null} readOnly />
+				<strong>
+					<p style={{ color: 'rgba(0,0,0,0.2)' }}>No Reviews</p>
+				</strong>
+			</div>
+		);
+	}
 
 	return reviews.map((review) => <Review review={review} />);
 }
